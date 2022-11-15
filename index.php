7012eb447ec5e9a30add101ac21ef40d2e636fec
Mariyatultyh <108410719+Mariyatultyh@users.noreply.github.com>
2022-11-15 18:57:22 +0700

    Tugas UTS
    upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Admin</title>
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <style>
+        .container {
+            display: flex;
+            justify-content: space-between;
+            padding: 30px;
+            width: 100%;
+        }
+        body {
+            padding: 20px 50px;
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+</head>
+
+<body style="background-image: url('https://img.freepik.com/free-vector/composed-particles-swirling-abstract-graphics_456031-137.jpg?size=626&ext=jpg&ga=GA1.2.833843270.1668478839&semt=sph');">
+    <div class="container">
+        <div>
+        <h1>Login DB Paswa</h1>
+        </div>
+        <div>
+            <a class="btn btn-primary" href="login.php">Login</a>
+            <a class="btn btn-primary" href="registrasi.php">Registrasi Peserta</a>
+        </div>
+    </div>
+</body>
+
+</html>
